5e3f227069953e736d119fb049bbc745a657caad
Hasan Huseyin Gumustepe <hgumustepe1@gmail.com>
2024-05-09 23:51:16 +0300

    Hata çözüldü resim dosya okundu

--- Shape_Detection.py
+++ Shape_Detection.py
@@ -1,32 +1,31 @@
 import cv2
 import numpy as np
 
-
 font = cv2.FONT_HERSHEY_SIMPLEX
-font = cv2.FONT_HERSHEY_COMPLEX #OPENCV_FONTS
+font = cv2.FONT_HERSHEY_COMPLEX
 
-image_path = "C:\Users\DELL\PycharmProjects\opencv_project\test_images"
+image_path = "C:\\Users\\DELL\\PycharmProjects\\opencv_project\\test_images\\polygons.png"
 img = cv2.imread(image_path)
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   #resmi 0 ile 1'lere ayrılır
-_,threshold = cv2.threshold(gray,240,255,cv2.THRESH_BINARY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, threshold = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
 
-contours,_ = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
-    epsilon = 0.01 * cv2.arcLength(cnt,True)
-    approx = cv2.approxPolyDP(cnt,epsilon,True)
+    epsilon = 0.01 * cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, epsilon, True)
 
-    cv2.drawContours(img,[approx],0,(0),5)
+    cv2.drawContours(img, [approx], 0, (0), 5)
 
-    x = approx.ravel()[0]#ravel fonksiyonu dik satırları satıra döker / 0 index x  1 index y işaret eder
-    y = approx.ravel()[1] # yatay sıralar her bir köşeyi temsil eder
+    x = approx.ravel()[0]
+    y = approx.ravel()[1]
 
     if len(approx) == 3:
-        cv2.putText(img,"Triangle",(x,y),font,1,(0))
+        cv2.putText(img, "Triangle", (x, y), font, 1, (0))
 
     elif len(approx) == 4:
-        cv2.putText(img,"Rectangle",(x,y),font,1,(0))
+        cv2.putText(img, "Rectangle", (x, y), font, 1, (0))
 
     elif len(approx) == 5:
         cv2.putText(img, "Pentagon", (x, y), font, 1, (0))
@@ -37,9 +36,6 @@ for cnt in contours:
     else:
         cv2.putText(img, "Ellipse", (x, y), font, 1, (0))
 
-
-cv2.imshow("IMG",img)
-
+cv2.imshow("IMG", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
